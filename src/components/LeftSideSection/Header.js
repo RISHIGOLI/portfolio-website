5e40531d67e930d17b1815e18789062ab5860460
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-07-17 17:23:58 +0530

    created OpenMenu drawer

--- src/components/LeftSideSection/Header.js
+++ src/components/LeftSideSection/Header.js
@@ -1,15 +1,23 @@
+import React, { useState } from 'react';
 import { Grid, Box, Button } from '@material-ui/core'
 import { MdMenu } from "react-icons/md";
 import Input from '../Input';
+import OpenMenuPopover from './OpenMenuPopover';
 
 function Header() {
+    const [openMenuPopover, setOpenMenuPopover] = useState(false)
     return (
-        <Grid style={{ height: '2.5rem', backgroundColor: '#FFFFFF', padding: '.375rem .8125rem .5rem .8125rem' }}>
-            <Grid style={{ width: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-                <Button style={{ width: '2.5rem', height: '2.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '1.5rem', minWidth: 'unset', borderRadius: '50%' }}><MdMenu /></Button>
-                <Box style={{ width: '90%', height: '2.5rem', marginLeft:'.8125rem' }}><Input /></Box>
+        <>
+            <Grid style={{ height: '2.5rem', backgroundColor: '#FFFFFF', padding: '.375rem .8125rem .5rem .8125rem' }}>
+                <Grid style={{ width: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                    <Button style={{ width: '2.5rem', height: '2.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '1.5rem', minWidth: 'unset', borderRadius: '50%' }} onClick={()=>setOpenMenuPopover(true)}><MdMenu /></Button>
+                    <Box style={{ width: '90%', height: '2.5rem', marginLeft: '.8125rem' }}><Input /></Box>
+                </Grid>
             </Grid>
-        </Grid>
+            {
+                openMenuPopover && <OpenMenuPopover open={openMenuPopover} onClose={()=>setOpenMenuPopover(false)}/>
+            }
+        </>
     )
 }
 
---
+++ src/components/LeftSideSection/OpenMenuPopover.js
@@ -0,0 +1,128 @@
+import { Grid, makeStyles } from '@material-ui/core'
+import Popover from '@mui/material/Popover'
+import { CiUser } from "react-icons/ci";
+import { FiPlayCircle } from "react-icons/fi";
+import { RiSettings3Line } from "react-icons/ri";
+import { RxMoon } from "react-icons/rx";
+import { MdAnimation } from "react-icons/md";
+import { FiHelpCircle } from "react-icons/fi";
+import { GoBug } from "react-icons/go";
+import { MdAddCircleOutline } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
+const useStyles = makeStyles((theme) => ({
+    popover: {
+        top: '3rem !important',
+        right: '8px !important',
+        borderRadius: '10px !important',
+        width: '225px !important',
+        height: 'max-content !important',
+        // opacity: '0.8 !important',
+        background: 'rgb(255,255,255,0.733) !important',
+        backdropFilter: 'blur(10px)',
+        minWidth: '17rem !important',
+        maxHeight: 'calc(100 * 6.41px - 3.75rem)',
+        padding: '0.25rem 0'
+    },
+    popoverItem: {
+        fontSize: '.875rem',
+        margin: '.125rem .25rem',
+        padding: '.25rem',
+        paddingInlineEnd: '.75rem',
+        borderRadius: '.375rem',
+        width: 'auto',
+        fontWeight: 500,
+        transform: 'scale(1)',
+        transition: '.15s ease-in-out transform',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        '&:hover': {
+            background: 'rgb(0,0,0,0.067)',
+            cursor: 'pointer'
+        }
+    },
+    itemIcon: {
+        fontSize: '1.25rem', color: 'rgb(112,117,121)', fontWeight: '900'
+    }
+}))
+
+function OpenMenuPopover({open,onClose}) {
+    const classes = useStyles()
+    const items = Array(12).fill(1)
+    const popoverItems = [
+        {
+            icon: <FaRegUser className={classes.itemIcon} />,
+            title: 'Contacts'
+        },
+        {
+            icon: <FiPlayCircle className={classes.itemIcon} />,
+            title: 'My Stories'
+        },
+        {
+            icon: <RiSettings3Line className={classes.itemIcon} />,
+            title: 'Settings'
+        },
+        {
+            icon: <RxMoon className={classes.itemIcon} />,
+            title: 'Night Mode'
+        },
+        {
+            icon: <MdAnimation className={classes.itemIcon} />,
+            title: 'Animations'
+        },
+        {
+            icon: <FiHelpCircle className={classes.itemIcon} />,
+            title: 'Telegram Features'
+        },
+        {
+            icon: <GoBug className={classes.itemIcon} />,
+            title: 'Report a Bug'
+        },
+        {
+            icon: <MdAddCircleOutline className={classes.itemIcon} />,
+            title: 'Install App '
+        }
+    ]
+    return (
+        <Popover
+            open={open}
+            onClose={onClose}
+            classes={{ paper: classes.popover }}
+        >
+            <Grid style={{ display: 'flex', flexDirection: 'column' }}>
+                {
+                    popoverItems.map((item, index) => (
+                        <Grid
+                            className={classes.popoverItem}
+                            key={index}
+                        >
+                            <Grid style={{
+                                maxWidth: '1.25rem',
+                                marginInline: '.5rem 1.25rem'
+                            }}>
+                                {item.icon}
+                            </Grid>
+                            <Grid >
+                                {item.title}
+                            </Grid>
+                        </Grid>
+                    ))
+                }
+                <Grid
+                    style={{
+                        padding: '.5rem 0',
+                        // background: 'rgb(244,244,245)',
+                        cursor: 'default',
+                        color: 'rgb(112,117,121)',
+                        fontSize: '.8125rem',
+                        textAlign: 'center'
+                    }}
+                >
+                    Telegram Web A 10.9.8
+                </Grid>
+            </Grid>
+        </Popover >
+    )
+}
+
+export default OpenMenuPopover
