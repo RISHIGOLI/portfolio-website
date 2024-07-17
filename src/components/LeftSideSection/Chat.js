d4680bea214d4111a9a1592ce1b2ba2822a2f56b
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-07-17 15:22:22 +0530

    some css changes in chat tab

--- src/components/LeftSideSection/Chat.js
+++ src/components/LeftSideSection/Chat.js
@@ -15,18 +15,18 @@ function Chat({ chat, selected }) {
         // main container
         <Button className={classes.mainContainer} style={{ color: selected && 'white', fontWeight: selected && '600' }}>
             {/* badge container */}
-            <Grid style={{ width: '20%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Box style={{ height: '54px', width: '54px', borderRadius: '50%', backgroundColor: randomColor, display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'whitesmoke', fontWeight: 'bold' }}>{chat.creator.name?.substring(0, 2).toUpperCase()}</Box>
+            <Grid style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Box style={{ height: '3.5rem', width: '3.5rem', borderRadius: '50%', backgroundColor: randomColor, display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'whitesmoke', fontWeight: 'bold' }}>{chat.creator.name?.substring(0, 2).toUpperCase()}</Box>
             </Grid>
             {/* info container */}
-            <Grid style={{ width: '80%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <Grid style={{ minWidth: '85%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                 <Grid style={{ width: '100%', display: 'flex', justifyContent: 'space-between', lineHeight: '1.5rem', height: 'fit-content' }}>
                     <Box style={{ marginLeft: '5px' }}>
                         <h3 style={{ fontSize: '1rem', margin: 0 }}>{chat.creator?.name || chat.creator.phone || "Nobel CHAT "}</h3>
                     </Box>
                     <Box style={{ marginRight: '5px', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '0.75rem', color: !selected && 'rgb(112,117,121)' }}>06:30 PM</Box>
                 </Grid>
-                <Grid style={{ width: '100%', margin: '0px 5px', display: 'flex', color: !selected && 'rgb(112,117,121)', lineHeight: '1.5rem', overflow: 'hidden' }}>
+                <Grid style={{ margin: '0px 5px', display: 'flex', color: !selected && 'rgb(112,117,121)', lineHeight: '1.5rem', overflow: 'hidden' }}>
                     <p style={{margin: 0, textWrap: 'nowrap'}}>message and message count row asda asdfa agasdgas asdf</p>
                 </Grid>
             </Grid>
