1aa1b2df0a33fd27e4740fdfc60a0f9d6f7579b1
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-07-18 19:49:15 +0530

    added chat tail svg to the messages

--- src/components/RightSection/ChatWindow.js
+++ src/components/RightSection/ChatWindow.js
@@ -41,13 +41,22 @@ function ChatWindow() {
                                 {
                                     message.sender_id === currentUserId ?
                                         <>
-                                            <Grid style={{ width: 'fit-content', maxWidth: '70%', height: 'auto', marginBottom: '5px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: '10px', backgroundColor: 'white', borderRadius: '15px 15px 15px 0px' }}>
-                                                {message.message}
-                                            </Grid></> :
-                                        <>
-                                            <Grid style={{ width: 'auto', height: 'auto', marginBottom: '5px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-                                                <Grid style={{ maxWidth: '70%', width: 'fit-content', display: 'flex', justifyContent: 'flex-end', backgroundColor: 'pink', padding: '10px', borderRadius: '15px 15px 0px 15px' }}>
+                                            <Grid style={{ width: 'fit-content', maxWidth: '70%', height: 'auto', marginBottom: '5px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: '10px', backgroundColor: 'white', borderRadius: '15px 15px 15px 0px', position: 'relative' }}>
+                                                <Grid style={{ width: 'fit-content', position: 'absolute', left: '-9px', bottom: '-7px' }}><svg width="9" height="20" class="svg-appendix"><defs><filter x="-50%" y="-14.7%" width="200%" height="141.2%" filterUnits="objectBoundingBox" id="messageAppendix"><feOffset dy="1" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset><feGaussianBlur stdDeviation="1" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur><feColorMatrix values="0 0 0 0 0.0621962482 0 0 0 0 0.138574144 0 0 0 0 0.185037364 0 0 0 0.15 0" in="shadowBlurOuter1"></feColorMatrix></filter></defs><g fill="none" fill-rule="evenodd"><path d="M3 17h6V0c-.193 2.84-.876 5.767-2.05 8.782-.904 2.325-2.446 4.485-4.625 6.48A1 1 0 003 17z" fill="#FFF" filter="url(#messageAppendix)"></path><path d="M3 17h6V0c-.193 2.84-.876 5.767-2.05 8.782-.904 2.325-2.446 4.485-4.625 6.48A1 1 0 003 17z" fill="#FFF" class="corner"></path></g></svg></Grid>
+                                                <Grid>
                                                     {message.message}
+                                                </Grid>
+                                            </Grid>
+                                        </> :
+                                        <>
+                                            <Grid style={{ width: '100%', display: 'flex', justifyContent: 'flex-end', marginBottom: '5px' }}>
+                                                <Grid style={{ maxWidth: '70%', width: 'fit-content', display: 'flex', justifyContent: 'flex-end', backgroundColor: 'pink', padding: '10px', borderRadius: '15px 15px 0px 15px', position: 'relative' }}>
+                                                    <Grid style={{ width: 'fit-content', position: 'absolute', right: '-9px', bottom: '-7px' }}><svg width="9" height="20" class="svg-appendix"><defs><filter x="-50%" y="-14.7%" width="200%" height="141.2%" filterUnits="objectBoundingBox" id="messageAppendix"><feOffset dy="1" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset><feGaussianBlur stdDeviation="1" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur><feColorMatrix values="0 0 0 0 0.0621962482 0 0 0 0 0.138574144 0 0 0 0 0.185037364 0 0 0 0.15 0" in="shadowBlurOuter1"></feColorMatrix></filter></defs><g fill="none" fill-rule="evenodd"><path d="M6 17H0V0c.193 2.84.876 5.767 2.05 8.782.904 2.325 2.446 4.485 4.625 6.48A1 1 0 016 17z" fill="pink" filter="url(#messageAppendix)"></path><path d="M6 17H0V0c.193 2.84.876 5.767 2.05 8.782.904 2.325 2.446 4.485 4.625 6.48A1 1 0 016 17z" fill="pink" class="corner"></path></g></svg></Grid>
+                                                    <Grid style={{ width: 'auto', height: 'auto', marginBottom: '5px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                                                        <Grid>
+                                                            {message.message}
+                                                        </Grid>
+                                                    </Grid>
                                                 </Grid>
                                             </Grid>
                                         </>
@@ -81,7 +90,7 @@ function ChatWindow() {
                 </Grid>
             </Grid>
             {
-                openMoreActionsPopover && <MoreActionsPopover open={openMoreActionsPopover} onClose={()=>setOpenMoreActionPopover(false)}/>
+                openMoreActionsPopover && <MoreActionsPopover open={openMoreActionsPopover} onClose={() => setOpenMoreActionPopover(false)} />
             }
         </Grid>
     )
